53a889992ba2d40c063e885d93987dc7d7e71030
unknown <sanjel.saree@gmail.com>
2022-03-13 23:29:33 -0500

    Initial

---
+++ src/main/java/com/kuebikoIT/ppratice/PpraticeApplication.java
@@ -0,0 +1,20 @@
+package com.kuebikoIT.ppratice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class PpraticeApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PpraticeApplication.class, args);
+
+
+    }
+
+}
---
+++ src/test/java/com/kuebikoIT/ppratice/PpraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuebikoIT.ppratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
